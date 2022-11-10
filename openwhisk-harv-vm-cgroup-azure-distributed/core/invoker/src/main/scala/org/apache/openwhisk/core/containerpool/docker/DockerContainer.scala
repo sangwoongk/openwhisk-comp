8addfd47500fd9edd84d7fbcf62a23851d700b48
Sangwoong Kim <sw.kim@dgist.ac.kr>
2022-11-10 16:58:26 +0900

    Fix cpu usage read error

--- openwhisk-harv-vm-cgroup-azure-distributed/core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/docker/DockerContainer.scala
+++ openwhisk-harv-vm-cgroup-azure-distributed/core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/docker/DockerContainer.scala
@@ -215,7 +215,7 @@ class DockerContainer(protected val id: ContainerId,
   // yanqi, path to read docker cpu usage (in ns)
   // protected val dockerCpuPath: String = "/cpu_docker/" + id.asString + "/cpuacct.usage"
   // for cgroup specifically
-  protected val dockerCpuPathOrigin: String = "/sys/fs/cgroup/cpu/docker" + id.asString + "/cpuacct.usage"
+  protected val dockerCpuPathOrigin: String = "/sys/fs/cgroup/cpu/docker/" + id.asString + "/cpuacct.usage"
   protected val dockerCpuPathSlice: String = "/sys/fs/cgroup/cpu/system.slice/docker-" + id.asString + ".scope/cpuacct.usage"
   protected val dockerCpuPath: String = if (Files.exists(Paths.get(dockerCpuPathOrigin))) dockerCpuPathOrigin else dockerCpuPathSlice
   // protected val dockerCpuPath: String = "/sys/fs/cgroup/cpu/cpuacct.usage"
